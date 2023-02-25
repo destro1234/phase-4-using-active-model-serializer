9791fb9afb9f2fc8492efff3b0a6708a978bdd1b
Elias <eliasventura23@gmail.com>
2023-02-25 11:36:34 -0500

    finished lab

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,9 +11,23 @@ class MoviesController < ApplicationController
     render json: movie
   end
 
+  def summary
+    movie = find_movie
+    render json: movie, serializer: MovieSummarySerializer
+  end
+
+  def summaries
+    movies = Movie.all
+    render json: movies, each_serializer: MovieSummarySerializer
+  end
+
   private
 
   def render_not_found_response
     render json: { error: "Movie not found" }, status: :not_found
+  end
+
+  def find_movie
+    Movie.find(params[:id])
   end
 end
---
+++ app/serializers/movie_summary_serializer.rb
@@ -0,0 +1,9 @@
+class MovieSummarySerializer < ActiveModel::Serializer
+
+attribute :summary
+
+    def summary
+        "#{self.object.title} - #{self.object.description[0..49]...}"
+    end
+
+end
